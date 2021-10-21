161169358476fbb5b5d858b2dcd5f5f90309890b
Adeyinka Adefolurin <folurinyinka@gmail.com>
2021-10-21 18:49:34 +0100

    chore: prepare the AdCampaign model + migration

---
+++ app/Models/AdCampaign.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class AdCampaign extends Model implements HasMedia
+{
+    use HasFactory;
+    use SoftDeletes;
+    use InteractsWithMedia;
+
+    /**
+     * Get the owning user of the ad campaign.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2021_10_21_065431_create_ad_campaigns_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdCampaignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ad_campaigns', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('name');
+            $table->timestamp('date_from');
+            $table->timestamp('date_to');
+            $table->float('total_budget_in_usd');
+            $table->float('daily_budget_in_usd');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ad_campaigns');
+    }
+}
