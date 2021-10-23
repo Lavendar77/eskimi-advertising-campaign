b0b0d91b77111e96dba9d3eba6fa0732c2a4cf50
Adeyinka Adefolurin <folurinyinka@gmail.com>
2021-10-23 20:33:38 +0100

    style: add some padding

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -12,7 +12,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <div class="flex justify-between mb-3">
+                        <div class="p-10 flex justify-between mb-3">
                             <h4 class="font-bold text-3xl">Your Ad Campaigns</h4>
 
                             <BreezeButton @click="createNewAdCampaign">Create New Ad Campaign</BreezeButton>
